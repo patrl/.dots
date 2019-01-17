df04941246bac65035d93a6fdbf61d4318347b0b
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-01-17 08:18:37 +0100

    Added words to dictionary

--- system/syncthing.nix
+++ system/syncthing.nix
@@ -1,12 +1,12 @@
 {config, pkgs, ... }:
 
-let
-  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
-in {
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    syncthing = unstable.syncthing;
-  };
+# let
+  # unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+# in {
+{
+  # nixpkgs.config.packageOverrides = pkgs: {
+    # syncthing = unstable.syncthing;
+  # };
 
 
   services.syncthing.enable = true;
