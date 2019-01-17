4b5fae9e9f571aeb9b78ee7ce0bd0a6368c77409
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-01-17 08:18:08 +0100

    geoclue doesn't work anymore

--- system/redshift.nix
+++ system/redshift.nix
@@ -4,20 +4,22 @@ let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in {
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    geoclue2 = unstable.geoclue2;
-  };
+  # nixpkgs.config.packageOverrides = pkgs: {
+    # geoclue2 = unstable.geoclue2;
+  # };
 
 
-  environment.systemPackages = with pkgs; [
-    unstable.redshift
-  ];
+  # environment.systemPackages = with pkgs; [
+    # unstable.redshift
+  # ];
 
 
-  services.geoclue2.enable= true;
+  # services.geoclue2.enable= true;
 
-  # services.redshift.enable = true;
-  # services.redshift.provider = "geoclue2";
+  services.redshift.enable = true;
+  services.redshift.provider = "manual";
+  services.redshift.latitude = "52.520008";
+  services.redshift.longitude = "13.404954";
   # services.geoclue2.enable = true;
 
   # environment.systemPackages = with pkgs; [
