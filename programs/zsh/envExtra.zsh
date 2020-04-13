f3cc6b84b8484d5e0ac566dc441e7e6a13145678
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-04-13 17:28:26 -0400

    Changed emacs dir location

--- programs/zsh/envExtra.zsh
+++ programs/zsh/envExtra.zsh
@@ -1,4 +1,4 @@
-  path+=('/home/patrl/.emacs.d/bin')
+  path+=('/home/patrl/.config/emacs/bin')
   path+=('/home/patrl/.local/bin')
 
   if [ -e /home/patrl/.nix-profile/etc/profile.d/nix.sh ]; then . /home/patrl/.nix-profile/etc/profile.d/nix.sh; fi
