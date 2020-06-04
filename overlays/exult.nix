d78c5df07d8121875f2183c8868277be98cd0a90
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-04 12:21:45 -0400

    Fixed exult overlay -- this is currently necessary until my PR is accepted

--- overlays/exult.nix
+++ overlays/exult.nix
@@ -1,11 +1,4 @@
 self: super:
 {
-  exult = super.exult.overrideAttrs ( old: rec {
-    name = "exult-1.6";
-    src = super.fetchurl {
-      url = "mirror://sourceforge/exult/exult-1.6.tar.gz";
-      sha256 = "1dm27qkxj30567zb70q4acddsizn0xyi3z87hg7lysxdkyv49s3s";
-      # date = 2020-04-30T00:37:49-0400;
-    };
-  });
+  exult16 = super.callPackage ./exult { };
 }
