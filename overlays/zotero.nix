ab49f82b216091343096f96c2b7268ab7f63e6e4
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-09-02 12:18:06 -0400

    Update zotero

--- overlays/zotero.nix
+++ overlays/zotero.nix
@@ -1,11 +1,11 @@
 self: super:
 {
   zotero = super.zotero.overrideAttrs ( old: rec {
-    version = "5.0.85";
+    version = "5.0.88";
     src = super.fetchurl {
-    url = "https://download.zotero.org/client/release/5.0.85/Zotero-5.0.85_linux-x86_64.tar.bz2";
-    sha256 = "0zqc27kld7rm3akmrnf9ba1x2hb9838cbv6i3nkqvg81ly5gfbxs";
-    # date = 2020-04-08T12:41:23-0400;
+    url = "https://download.zotero.org/client/release/5.0.88/Zotero-5.0.88_linux-x86_64.tar.bz2";
+    sha256 = "19r9jmakr04raqripfnqm2b9gwpi52lklrrqgqyb1x35a4xvnj62";
+    # date = 2020-07-12T13:44:52-0400;
   };
   });
 }
