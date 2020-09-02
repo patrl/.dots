7e06df852073c2d86ea84a72561f65196825ab24
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-09-02 12:19:33 -0400

    Update pure

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -22,16 +22,16 @@
     llfu = "exa -bghHliS --git";
     prev = ''fzf --preview "bat --color always {}"'';
    };
-   plugins = [
-     {
-     name = "pure";
-      src = pkgs.fetchFromGitHub {
-        owner = "sindresorhus";
-        repo = "pure";
-        rev = "957e07101a1c14502d32a25d9fd327c83098bf3c";
-        # date = 2020-01-07T23:52:01+07:00;
-        sha256 = "0dmr0b4x903r28w74b8w0vdgmlckhi9n31k26ld24hh2s74g57bk";
-      };
-     }
-    ];
+   # plugins = [
+   #   {
+   #   name = "pure";
+   #    src = pkgs.fetchFromGitHub {
+   #      owner = "sindresorhus";
+   #      repo = "pure";
+   #      rev = "957e07101a1c14502d32a25d9fd327c83098bf3c";
+   #      # date = 2020-01-07T23:52:01+07:00;
+   #      sha256 = "0dmr0b4x903r28w74b8w0vdgmlckhi9n31k26ld24hh2s74g57bk";
+   #    };
+   #   }
+   #  ];
 }
